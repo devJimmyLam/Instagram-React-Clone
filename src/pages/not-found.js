44e113acf2eafad7a5aeb97bc99c4856b0daa59c
Jimmy Lam <dev.jimmylam@gmail.com>
2022-03-02 01:00:58 -0800

    added not found page and styling

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,36 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import Layout from "../components/shared/Layout";
+import {Link } from 'react-router-dom';
 
 function NotFoundPage() {
-	return <div>NotFoundPage</div>;
+	return (
+		<Layout 
+			title="Page Not Found"
+			marginTop={120}
+			>
+			<Typography
+				variant="h5"
+				align="center"
+				paragraph
+			>
+				Sorry, this page isn't available.
+				<Typography align="center">
+					The link you followed may be broken, or the page may have been removed. 
+					<Link to="/" style={{ textDecoration: 'none' }}>
+						{" "}
+						<Typography
+							color="primary"
+							component="span"
+						>
+							Go to back to Instagram.
+						</Typography>
+					</Link>
+				</Typography>
+			</Typography>
+
+		</Layout>
+	)
 }
 
 export default NotFoundPage;
